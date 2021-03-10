65b90d3ce93efd4383d2e6211f508272abcca430
EX-MG-TX <80203485+EX-MG-TX@users.noreply.github.com>
2021-03-10 17:28:29 +0800

    Update watch_task.py

--- tasks/watch_task.py
+++ tasks/watch_task.py
@@ -18,8 +18,7 @@ async def watch_task(biliapi: asyncbili) -> None:
     ids = ret["data"]["archives"]
 
     try:
-        ret = await biliapi.report(204516234, 307178951, 500)
-	ret2 = await biliapi.report(756904275, 304166415, 500)
+        ret = await biliapi.report(204516234, 307178951, 300)
         if ret["code"] == 0:
             logging.info(f'{biliapi.name}: 成功模拟观看av号为{ids[5]["aid"]}的视频')
         else:
